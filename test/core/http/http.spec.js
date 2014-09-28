e11a2d9f128271b3c1da4386ddbcb80eeea9fb39
Sebastien Couture <sebastiencouture>
2014-09-27 19:15:45 -0700

    basic setup of http spec

--- test/core/http/http.spec.js
+++ test/core/http/http.spec.js
@@ -4,4 +4,56 @@ describe("$http", function() {
     it("should be invokable", function() {
 
     });
+
+    describe("options", function() {
+
+    });
+
+    describe("headers", function() {
+
+    });
+
+    describe("url", function() {
+
+    });
+
+    describe("data", function() {
+
+    });
+
+    describe("serialize", function() {
+
+    });
+
+    describe("promise", function() {
+
+    });
+
+    describe("parse", function() {
+
+    });
+
+    describe("get", function() {
+
+    });
+
+    describe("post", function() {
+
+    });
+
+    describe("jsonp", function() {
+
+    });
+
+    describe("head", function() {
+
+    });
+
+    describe("put", function() {
+
+    });
+
+    describe("patch", function() {
+
+    });
 });
