1d07f845fb0604442e4425d3e50a583fff2b9018
Sebastien Couture <sebastiencouture>
2014-10-08 20:56:59 -0700

    described mock spec

--- test/mock/mock.spec.js
+++ test/mock/mock.spec.js
@@ -2,4 +2,31 @@
 
 describe("recurveMock", function() {
 
+    it("should instantiate core services", function() {
+
+    });
+
+    it("should instantiate defined services of included module", function() {
+
+    });
+
+    it("should include modules in order so expected override rules are applied", function() {
+
+    });
+
+    it("should only instantiate defined services and dependent services", function() {
+
+    });
+
+    it("should re-instantiate all services if $invoke called again", function() {
+
+    });
+
+    it("should allow to override services/decorators in module", function() {
+
+    });
+
+    it("should allow to override core services/decorators", function() {
+
+    });
 });
