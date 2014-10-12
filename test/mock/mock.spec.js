29eff4a112506611841e615397ad87b496e9d1b7
Sebastien Couture <sebastiencouture>
2014-10-12 09:13:54 -0700

    added core mock module tests

--- test/mock/mock.spec.js
+++ test/mock/mock.spec.js
@@ -3,30 +3,100 @@
 describe("recurveMock", function() {
 
     it("should instantiate core services", function() {
-
+        $invoke(["$window", "$promise"], function($window, $promise) {
+            expect($window).toBeDefined();
+            expect($promise).toBeDefined();
+        })
     });
 
-    it("should instantiate defined services of included module", function() {
+    it("should instantiate services of included module", function() {
+        var module = recurve.module();
+        module.value("$a", 1);
 
+        $include(module);
+        $invoke(["$a"], function($a) {
+            expect($a).toEqual(1);
+        });
+    });
+
+    it("should include multiple modules", function() {
+        var module = recurve.module();
+        module.value("$a", 1);
+
+        var module2 = recurve.module();
+        module.value("$b", 2);
+
+        $include(module);
+        $include(module2);
+
+        $invoke(["$a", "$b"], function($a, $b) {
+            expect($a).toEqual(1);
+            expect($b).toEqual(2);
+        });
     });
 
     it("should include modules in order so expected override rules are applied", function() {
+        var module = recurve.module();
+        module.value("$a", 1);
 
+        var module2 = recurve.module();
+        module.value("$a", 2);
+
+        $include(module);
+        $include(module2);
+
+        $invoke(["$a"], function($a) {
+            expect($a).toEqual(2);
+        });
     });
 
     it("should only instantiate defined services and dependent services", function() {
+        var module = recurve.module();
+        module.value("$a", 1);
 
+        var serviceProvider = jasmine.createSpy("serviceProvider");
+        module.factory("$b", null, serviceProvider);
+
+        $include(module);
+        $invoke(["$a"], function($a) {
+            expect(serviceProvider).not.toHaveBeenCalled();
+        });
     });
 
     it("should re-instantiate all services if $invoke called again", function() {
+        var module = recurve.module();
+        var serviceProvider = jasmine.createSpy("serviceProvider").and.returnValue(1);
+        module.factory("$a", null, serviceProvider);
 
+        $include(module);
+        $invoke(["$a"], function() {
+            expect(serviceProvider).toHaveBeenCalled();
+        });
+        $invoke(["$a"], function() {
+            expect(serviceProvider.calls.count()).toEqual(2);
+        });
     });
 
     it("should allow to override services/decorators in module", function() {
+        var module = recurve.module();
+        module.value("$a", 1);
 
+        $include(module, function($mockable) {
+            $mockable.value("$a", 2);
+        });
+
+        $invoke(["$a"], function($a) {
+            expect($a).toEqual(2);
+        });
     });
 
     it("should allow to override core services/decorators", function() {
+        $include(null, function($mockable) {
+            $mockable.value("$log", {});
+        });
 
+        $invoke(["$log"], function($log) {
+            expect($log).toEqual({});
+        });
     });
 });
