eb327915ec4adf7c5e59a865c1989753ad5e408d
Sebastien Couture <sebastiencouture>
2014-10-13 10:35:18 -0700

    initial setup of router interface

--- src/router/router.js
+++ src/router/router.js
@@ -5,8 +5,51 @@
     recurve.router = {};
     var module = recurve.router.$module = recurve.module();
 
-    module.factory("$router", null, function() {
+    // window "hashchange"
+    // window "popstate"
 
+    // if want to support IE8/9... require polyfill?
+    // https://github.com/devote/HTML5-History-API
+
+    module.factory("$router", null, function() {
+        return {
+            match: function(path) {
+                return {
+                    to: function(callback) {
+
+                    },
+
+                    before: function(callback) {
+
+                    },
+
+                    after: function(callback) {
+
+                    }
+                };
+            },
+
+            navigate: function(path) {
+
+            },
+
+            replace: function(path) {
+
+            },
+
+            reload: function() {
+
+            },
+
+            start: function() {
+
+            }
+        };
+    });
+
+    module.config("$router", {
+        mode: "history",
+        root: ""
     });
 })();
 
