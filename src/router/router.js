82c128b9fffd60adcc83d135638b37aaa4b820f3
Sebastien Couture <sebastiencouture>
2014-10-23 20:21:42 -0700

    removed history service not needed (rely on polyfill), wip router

--- src/router/router.js
+++ src/router/router.js
@@ -6,16 +6,16 @@
     recurve.router = {};
     var module = recurve.router.$module = recurve.module();
 
-    addRouterHistoryService(module);
-
-    module.factory("$router", ["$window", "$history", "$config"], function($window, $history, $config) {
+    module.factory("$router", ["$window", "$config"], function($window, $config) {
         // Support for history polyfill
         // https://github.com/devote/HTML5-History-API
-        var location = $window.history.location || $window.location;
+        var history = $window.history;
+        var location = history.location || $window.location;
 
-        var handlers = [];
-        var noMatchHandler;
+        var routes = [];
+        var noMatchRoute;
         var currentPath = removeRoot(location.href);
+        var started;
 
         function decode(str) {
             if (!str) {
@@ -23,40 +23,6 @@
             }
 
             return decodeURIComponent(str).replace(/\+/g, " ");
-        }
-
-        function checkUrl() {
-            var path = removeRoot(location.href);
-            if(currentPath === path) {
-                return;
-            }
-
-            currentPath = path;
-            var handled = false;
-
-            recurve.forEach(handlers, function(handler) {
-                var params = handler.path.exec(currentPath);
-                if (params) {
-                    // full path
-                    params.shift();
-
-                    var decodedParams = [];
-                    recurve.forEach(params, function(param) {
-                        decodedParams.push(decode(param));
-                    });
-
-                    recurve.forEach(handler.callbacks, function(callback) {
-                        callback.apply(null, decodedParams);
-                    });
-
-                    handled = true;
-                    return false;
-                }
-            });
-
-            if (!handled && noMatchHandler) {
-                noMatchHandler();
-            }
         }
 
         var PATH_NAME_MATCHER = /:([\w\d]+)/g;
@@ -67,20 +33,81 @@
                 return path;
             }
 
+            return new RegExp(path.replace(PATH_NAME_MATCHER, PATH_REPLACER) + "$");
+        }
+
+        function pathParams(path) {
+            if (recurve.isRegExp(path)) {
+                return null;
+            }
+
             // reset index...IE :T
             PATH_NAME_MATCHER.lastIndex = 0;
 
             var params = [];
             var match = PATH_NAME_MATCHER.exec(path);
 
-            // TODO TBD pass these param names back on object or just provide them as parameters with the callback
-            // function?
             while(null !== match) {
                 params.push(match[1]);
                 match = PATH_NAME_MATCHER.exec(path);
             }
 
-            return new RegExp(path.replace(PATH_NAME_MATCHER, PATH_REPLACER) + "$");
+            return params;
+        }
+
+        function getRoute(pathRegExp) {
+            var found = null;
+            recurve.forEach(routes, function(route) {
+                if(recurve.areEqual(pathRegExp, route.pathRegExp)) {
+                    found = route;
+                    return false;
+                }
+            });
+
+            return found;
+        }
+
+        function addRoute(path) {
+            var pathRegExp = pathToRegExp(path);
+            var route = getRoute(pathRegExp);
+
+            if(!route) {
+                route.pathRegExp = pathRegExp;
+                route.params = pathParams(path);
+                route.callbacks = [];
+
+                route.handle = function(path) {
+                    var params = this.pathRegExp.exec(path);
+                    if (!params) {
+                        return false;
+                    }
+
+                    // full path
+                    params.shift();
+
+                    var decodedParams = {};
+                    recurve.forEach(params, function(param, index) {
+                        var decodedParam = decode(param);
+                        if (this.params[index]) {
+                            decodedParams[this.params[index]] = decodedParam;
+                        }
+                        else {
+                            decodedParams.splat = decodedParams.splat || [];
+                            decodedParams.splat.push(decodedParam);
+                        }
+                    });
+
+                    recurve.forEach(this.callbacks, function(callback) {
+                        callback.apply(null, decodedParams);
+                    });
+
+                    return true;
+                }
+
+                routes.push(route);
+            }
+
+            return routes;
         }
 
         function addRoot(path) {
@@ -99,77 +126,80 @@
             return path;
         }
 
-        function getHandler(path) {
-            var found;
-            recurve.forEach(handlers, function(handler) {
-                if(recurve.areEqual(path, handler.path)) {
-                    found = handler;
-                    return false;
-                }
-            });
-
-            return found;
-        }
-
-        function addHandler(path) {
-            path = pathToRegExp(path);
-            var handler = getHandler(path);
-
-            if(!handler) {
-                handler.path = path;
-                handler.callbacks = [];
-
-                handlers.push(handler);
+        function checkCurrentPath() {
+            var path = removeRoot(location.href);
+            if(currentPath === path) {
+                return;
             }
 
-            return handler;
+            currentPath = path;
+
+            var handled = false;
+            recurve.forEach(routes, function(route) {
+                handled = route.handle(path);
+                return !handled;
+            });
+
+            if (!handled && noMatchRoute) {
+                noMatchRoute();
+            }
         }
 
         return {
             match: function(path) {
-                var handler = addHandler(path);
+                var route = addRoute(path);
 
                 return {
                     to: function(callback) {
-                        handler.callbacks.push(callback);
+                        route.callbacks.push(callback);
                     }
                 };
             },
 
             noMatch: function(callback) {
-                noMatchHandler = callback;
+                noMatchRoute = callback;
             },
 
-            navigate: function(path, trigger) {
+            navigate: function(path, obj, trigger) {
                 path = addRoot(path);
                 trigger = trigger || true;
 
-                $history.pushState(null, null, path);
-                if (trigger) {
-                    checkUrl();
+                history.pushState(obj, null, path);
+                if (trigger && started) {
+                    checkCurrentPath();
                 }
             },
 
-            replace: function(path, trigger) {
+            replace: function(path, obj, trigger) {
                 path = addRoot(path);
                 trigger = trigger || true;
 
-                $history.replaceState(null, null, path);
-                if (trigger) {
-                    checkUrl();
+                history.replaceState(obj, null, path);
+                if (trigger && started) {
+                    checkCurrentPath();
                 }
+            },
+
+            back: function() {
+                history.back();
+            },
+
+            forward: function() {
+                history.forward();
+            },
+
+            go: function(delta) {
+                history.go(delta);
             },
 
             reload: function() {
                 currentPath = null;
-                checkUrl();
+                checkCurrentPath();
             },
 
             start: function() {
-                currentPath = location.href;
-
-                $history.popped.on(checkUrl);
-                $history.start();
+                started = true;
+                recurve.addEvent($window, "popstate", checkCurrentPath);
             }
         };
     });
