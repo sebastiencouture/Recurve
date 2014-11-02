40a9ee55a25a990b4112dbe6a0094a50f53c88c8
Sebastien Couture <sebastiencouture>
2014-11-02 10:17:21 -0800

    added comments for possible functionality to implement

--- src/flux/rest-action-creator.js
+++ src/flux/rest-action-creator.js
@@ -2,6 +2,9 @@
 
 function addRestActionCreatorService(module) {
     module.factory("$restActionCreator", ["$http", "$actionCreator"], function($http, $actionCreator) {
+        // functionality:
+        // - define resources for REST api
+        // - trigger success, error, and cancelled actions
         return {
 
         };
--- src/flux/state-action-creator.js
+++ src/flux/state-action-creator.js
@@ -3,6 +3,14 @@
 function addStateActionCreatorService(module) {
     // TODO TBD move $router to core recurve?
     module.factory("$stateActionCreator", ["$router", "$actionCreator"], function($router, $actionCreator) {
+        // functionality:
+        // - define states to a route
+        // - define modals to a route with default state (if one doesn't exist)
+        // - hooks:
+        // a. validate(done) => done to continue...parameters: false => go back, route => change route to
+        // a. getData(done) => done to continue...parameters: false => go back, route => change route to
+        //
+        // TODO TBD support child states??
         return {
 
         };
