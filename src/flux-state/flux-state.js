5fcb453fcf2719a71cfca139a764daea36fb9038
Sebastien Couture <s_couture_1@hotmail.com>
2014-12-11 21:15:02 -0800

    setup interface for flux state

--- src/flux-state/flux-state.js
+++ src/flux-state/flux-state.js
@@ -7,14 +7,24 @@
     module.factory("$state", ["$router", "$action"], function($router, $action) {
         // functionality:
         // - define states to a route
-        // - define modals to a route with default state (if one doesn't exist)
         // - hooks:
         // a. validate(done) => done to continue...parameters: false => go back, route => change route to
         // a. getData(done) => done to continue...parameters: false => go back, route => change route to
-        //
-        // TODO TBD support child states??
         return {
+            changeAction: $action(),
+            preChangeAction: $action(),
 
+            add: function(name, path, onValidate, onGetData) {
+                recurve.assert(name, "state name must be set");
+                recurve.assert(path, "state path must be set");
+
+                // child states
+                return {
+                    add: function(name, path, onValidate, onGetData) {
+
+                    }
+                }
+            }
         };
     });
 })();
