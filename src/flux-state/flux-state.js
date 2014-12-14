3924f7857fbbe7b5e12102d8f0975fdb211ef51d
Sebastien Couture <sebastiencouture>
2014-12-14 11:29:30 -0800

    wip $state

--- src/flux-state/flux-state.js
+++ src/flux-state/flux-state.js
@@ -4,23 +4,79 @@
     recurve.flux.state = {};
     var module = recurve.flux.state.$module = recurve.module();
 
+    // TODO TBD state date store => might be too app dependent?
+    // current
+    // previous
+    // error
+    // startChange
+    //
+    // action startChange => update startChange, clear error
+    // action change => clear startChange + error, update current + previous
+    // action error => clear startChange, update error
+
     module.factory("$state", ["$router", "$action", "$config"], function($router, $action, $config) {
+        var states = [];
+
         $router.setRoot($config.rootUrl);
 
         recurve.forEach($config.states, function(stateConfig) {
             recurve.assert(stateConfig.name, "state name must be set", stateConfig);
             recurve.assert(stateConfig.path, "state path must be set", stateConfig);
 
+            validateParent(stateConfig.name);
+
+            var path = "";
+            var parentState = getParent(stateConfig.name);
+            if (parentState) {
+                path = parentState.path + stateConfig.path;
+            }
+
+            addState(stateConfig.name, path);
+
+            $router.on(path, function(params) {
+                $state.startChangeAction.trigger();
+
+                resolve().then(function() {
+                    $state.changeAction.trigger();
+                }, function() {
+                    $state.errorAction.trigger();
+                });
+            });
+
             // stateConfig.name
-            // stateConfig.url
+            // stateConfig.path
             // stateConfig.data
             // stateConfig.resolve => set of objects, if any promises then wait... this is included with stateConfig.data
 
             // $router.on(stateConfig.path, handler);
         });
 
-        return {
-            preChangeAction: $action(),
+        function getParent(childName) {
+
+        }
+
+        function hasParent(childName) {
+            return -1 < childName.indexOf(".")
+        }
+
+        function validateParent(childName) {
+            if (!hasParent(childName)) {
+                return;
+            }
+
+            recurve.assert(getParent(childName), "no parent exists for state '{0}'", childName);
+        }
+
+        function addState(name, path) {
+
+        }
+
+        function resolve() {
+
+        }
+
+        var $state = {
+            startChangeAction: $action(),
             changeAction: $action(),
             errorAction: $action(),
 
@@ -31,6 +87,7 @@
                 // replace
             },
 
+            // convenience methods
             back: function() {
                 $router.back();
             },
@@ -47,6 +104,8 @@
                 $router.start();
             }
         };
+
+        return $state;
     });
 
     module.config("$state", {
