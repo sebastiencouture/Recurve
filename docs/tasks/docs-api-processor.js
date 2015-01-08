36d199640827c7f1bfb97d865d7f7e5852c94141
Sebastien Couture <sebastiencouture>
2015-01-07 19:31:39 -0800

    moved comments processing to separate module, make it easier for testing

---
+++ docs/tasks/docs-api-processor.js
@@ -0,0 +1,129 @@
+"use strict";
+
+var fileStream = require("fs-extra");
+var markdown = require("marked");
+
+
+function getExamplePath(examplePath, filePath, options) {
+    var noRoot = filePath.split(options.input)[1];
+    var noExtension = noRoot.split(".")[0];
+
+    return options.api.examples + noExtension + "/" + examplePath;
+}
+
+
+module.exports = {
+    processComments: function(comments, filePath, options) {
+        var processed = [];
+        comments.forEach(function(comment) {
+            var processedComment = {};
+            processedComment.description = this.processInternalLinks(comment.description, options.api.baseUrl);
+            processedComment.line = comment.line;
+            processedComment.codeStart = comment.codeStart;
+
+            this.processTags(processedComment, comment.tags, filePath, options);
+
+            processed.push(processedComment);
+        }, this);
+
+        return processed;
+    },
+
+    processTags: function(processedComment, tags, filePath, options) {
+        var baseUrl = options.api.baseUrl;
+
+        tags.forEach(function(tag) {
+            switch (tag.type) {
+                case "rdoc":
+                case "name":
+                case "module":
+                case "kind":
+                    processedComment[tag.type] = tag.string;
+                    break;
+                case "require":
+                    processedComment.requires = processedComment.requires || [];
+                    processedComment.requires.push(tag.string);
+                    break;
+                case "example":
+                    processedComment.examples = processedComment.examples || [];
+                    var path = getExamplePath(tag.string, filePath, options);
+                    var code = fileStream.readFileSync(path, "utf8");
+                    processedComment.examples.push({
+                        path: path,
+                        code: code
+                    });
+                    break;
+                case "description":
+                    processedComment.description = markdown(tag.string);
+                    processedComment.description = this.processInternalLinks(processedComment.description, baseUrl);
+                    break;
+                case "param":
+                    processedComment.params = processedComment.params || [];
+                    processedComment.params.push({
+                        name: tag.name,
+                        description: this.processInternalLinks(tag.description, baseUrl),
+                        types: tag.types
+                    });
+                    break;
+                case "throws":
+                    processedComment.throws = {
+                        description: this.processInternalLinks(tag.description, baseUrl),
+                        types: tag.types
+                    };
+                    break;
+                case "return":
+                case "returns":
+                    processedComment.returns = {
+                        description: this.processInternalLinks(tag.description, baseUrl),
+                        types: tag.types
+                    };
+                    break;
+                case "private":
+                    processedComment.isPrivate = true;
+                    break;
+                default:
+                    processedComment.tags = processedComment.tags || [];
+                    processedComment.tags.push({
+                        type: tag.type,
+                        name: tag.name,
+                        description: this.processInternalLinks(tag.description, baseUrl),
+                        types: tag.types
+                    });
+                    break;
+            }
+        }, this);
+    },
+
+    processInternalLinks: function (description, baseUrl) {
+        if (!description) {
+            return description;
+        }
+
+        // input:
+        // 1. {@link module.name description}
+        // 2. {@link module.name}
+        // 3. {@link module description}
+        // 4. {@link module}
+        // output:
+        // <a href="baseUrl + /module/name">description</a>
+
+        var processedDescription = description;
+        var regExp = /\{@link(.*?)\}/g;
+        var match;
+
+        while (match = regExp.exec(description)) {
+            var original = match[0];
+            var linkSplit = match[1].trim().split(" ");
+            var path = linkSplit.shift();
+            var pathSplit = path.split(".");
+            var module = pathSplit[0];
+            var name = pathSplit[1];
+            var description = linkSplit.join(" ") || path;
+
+            var anchor = '<a href="' + baseUrl + "/" + module + "/" + name + '">' + description + "</a>";
+            processedDescription = processedDescription.replace(original, anchor);
+        }
+
+        return processedDescription;
+    }
+};
--- docs/tasks/docs-api.js
+++ docs/tasks/docs-api.js
@@ -3,7 +3,7 @@
 var fileStream = require("fs-extra");
 var path = require("path");
 var dox = require("dox");
-var markdown = require("marked");
+var processor = require("./docs-api-processor");
 
 
 function generateDirectory(directoryPath, options) {
@@ -26,124 +26,10 @@ function generateFile(filePath, options) {
 
     var content = fileStream.readFileSync(filePath, "utf8");
     var rawComments = dox.parseComments(content, {skipSingleStar: true});
-    var processedComments = processComments(rawComments, filePath, options);
+    var processedComments = processor.processComments(rawComments, filePath, options);
 
     var outputPath = getOutputPath(filePath, options);
     fileStream.outputJsonSync(outputPath, processedComments);
-}
-
-function processComments(comments, filePath, options) {
-    var processed = [];
-    comments.forEach(function(comment) {
-        var processedComment = {};
-        processedComment.description = processInternalLinks(comment.description, options.api.baseUrl);
-        processedComment.line = comment.line;
-        processedComment.codeStart = comment.codeStart;
-
-        processTags(processedComment, comment.tags, filePath, options);
-
-        processed.push(processedComment);
-    });
-
-    return processed;
-}
-
-function processTags(processedComment, tags, filePath, options) {
-    var baseUrl = options.api.baseUrl;
-
-    tags.forEach(function(tag) {
-        switch (tag.type) {
-            case "rdoc":
-            case "name":
-            case "module":
-            case "kind":
-                processedComment[tag.type] = tag.string;
-                break;
-            case "require":
-                processedComment.requires = processedComment.requires || [];
-                processedComment.requires.push(tag.string);
-                break;
-            case "example":
-                processedComment.examples = processedComment.examples || [];
-                var path = getExamplePath(tag.string, filePath, options);
-                var code = fileStream.readFileSync(path, "utf8");
-                processedComment.examples.push({
-                    path: path,
-                    code: code
-                });
-                break;
-            case "description":
-                processedComment.description = markdown(tag.string);
-                processedComment.description = processInternalLinks(processedComment.description, baseUrl);
-                break;
-            case "param":
-                processedComment.params = processedComment.params || [];
-                processedComment.params.push({
-                    name: tag.name,
-                    description: processInternalLinks(tag.description, baseUrl),
-                    types: tag.types
-                });
-                break;
-            case "throws":
-                processedComment.throws = {
-                    description: processInternalLinks(tag.description, baseUrl),
-                    types: tag.types
-                };
-                break;
-            case "return":
-            case "returns":
-                processedComment.returns = {
-                    description: processInternalLinks(tag.description, baseUrl),
-                    types: tag.types
-                };
-                break;
-            case "private":
-                processedComment.isPrivate = true;
-                break;
-            default:
-                processedComment.tags = processedComment.tags || [];
-                processedComment.tags.push({
-                    type: tag.type,
-                    name: tag.name,
-                    description: processInternalLinks(tag.description, baseUrl),
-                    types: tag.types
-                });
-                break;
-        }
-    });
-}
-
-function processInternalLinks(description, baseUrl) {
-    if (!description) {
-        return description;
-    }
-
-    // input:
-    // 1. {@link module.name description}
-    // 2. {@link module.name}
-    // 3. {@link module description}
-    // 4. {@link module}
-    // output:
-    // <a href="baseUrl + /module/name">description</a>
-
-    var processedDescription = description;
-    var regExp = /\{@link(.*?)\}/g;
-    var match;
-
-    while (match = regExp.exec(description)) {
-        var original = match[0];
-        var linkSplit = match[1].trim().split(" ");
-        var path = linkSplit.shift();
-        var pathSplit = path.split(".");
-        var module = pathSplit[0];
-        var name = pathSplit[1];
-        var description = linkSplit.join(" ") || path;
-
-        var anchor = '<a href="' + baseUrl + "/" + module + "/" + name + '">' + description + "</a>";
-        processedDescription = processedDescription.replace(original, anchor);
-    }
-
-    return processedDescription;
 }
 
 function getFileExtension(filePath) {
@@ -155,13 +41,6 @@ function getOutputPath(filePath, options) {
     var noExtension = noRoot.split(".")[0];
 
     return options.api.output + noExtension + ".json";
-}
-
-function getExamplePath(examplePath, filePath, options) {
-    var noRoot = filePath.split(options.input)[1];
-    var noExtension = noRoot.split(".")[0];
-
-    return options.api.examples + noExtension + "/" + examplePath;
 }
 
 
