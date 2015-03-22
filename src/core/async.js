7144b608abb2e9466c06481c337555927f91ea75
Sebastien Couture <s_couture_1@hotmail.com>
2015-03-22 16:04:04 -0700

    clean up

--- src/core/async.js
+++ src/core/async.js
@@ -4,10 +4,8 @@
  * @rdoc service
  * @name $async
  * @module core
- * @example async-demo.js this is example 1 this is example 1 this is example 1 this is example 1 this is example 1 v this is example 1 321321312
+ * @example async-demo.js this is example 1
  * @example async-demo-2.js this is example 2
- * @require something
- * @require something2
  * @description
  * this is the description for $async!!
  ```
@@ -15,7 +13,6 @@
     };
  ```
  * [inline link](www.google.com)
- * @require something 3
  */
 
 "use strict";
@@ -35,37 +32,15 @@ function addAsyncService(module) {
              * @service $async
              * @name cancel
              *
-             * @param {String} id this is a value :)
-             * @param {Object} test not here!
-             * @return {String} some value [inline link](www.google.com) @{core.$promise#all promise something}
-             * @throws {Error} nothing!
-             * @private
-             * @description test
-             *
-             * description
+             * @param {String} id
+             * @param {Object} test
+             * @return {String} test @{core.$promise#all promise something}
+             * @description
+             * test
              */
             cancel: function(id) {
                 window.clearTimeout(id);
             }
-
-            /**
-             * @rdoc config
-             * @module core
-             * @service $async
-             * @name something
-             *
-             * @description blah
-             * @type {String}
-             */
-
-            /**
-             * @rdoc property
-             * @service $async
-             * @name something
-             *
-             * @description wee
-             * @type {String}
-             */
         });
     });
 }
